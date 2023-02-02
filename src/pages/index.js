b32e377cf019a079f24bcaf5925dfe2ce6f29635
stefanagloginic <stefana.gloginic@gmail.com>
2023-02-01 23:18:59 -0800

    [fixImports] test imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <StickyProvider>
         <Layout>
-          test
+          testing
         </Layout>
       </StickyProvider>
     </ThemeProvider>
