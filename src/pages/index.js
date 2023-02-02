c7b20cc07d60e68cd38ac47f594b5cafe04c2a4a
stefanagloginic <stefana.gloginic@gmail.com>
2023-02-01 23:35:56 -0800

    whats up

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,26 @@ export default function IndexPage() {
         <Layout>
           <SEO title="Project Footwear" />
           <Banner />
-          <KeyFeature />
+          <CoreFeature />
+          {
+            false ? (
+              <>
+                <Feature />
+                <PartnerSection />
+              </>
+            ) : null
+          }
+          <WorkFlow />
+          {
+            false ? (
+              <>
+                <TestimonialCard />
+                <SecurePayment />
+              </>
+            ) : null
+          }
+          <Package />
+          { false ? <Faq /> : null }
         </Layout>
       </StickyProvider>
     </ThemeProvider>
